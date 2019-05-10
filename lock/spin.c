7ac3b6ef00080becc7875589b697ab3080cfe76b
Wenyu Qi <wenyuqi@cs.uchicago.edu>
2019-05-10 16:01:46 -0500

    spinlock completed and tested

---
+++ lock/spin.c
@@ -0,0 +1,36 @@
+#import "spin.h"
+#import <pthread.h>
+
+int i = 0;
+spinlock_t lock;
+
+void* do_1();
+void* do_2();
+
+void* do_1() {
+    spinlock_acquire(&lock);
+    sleep(5);
+    printf("do_1: %d\n", i ++);
+    spinlock_release(&lock);
+}
+
+void* do_2() {
+    spinlock_acquire(&lock);
+    sleep(1);
+    printf("do_2: %d\n", i ++);
+    spinlock_release(&lock);
+}
+
+
+
+
+int main (){
+    pthread_t t1;
+    pthread_t t2;
+    pthread_create(&t1, NULL, do_1, NULL);
+    pthread_create(&t2, NULL, do_2, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    return 0;
+}
+
